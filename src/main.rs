88bcdb252b3908696fa96ae177963f50816132b3
hectorgrey <parker321@gmail.com>
2023-03-23 22:05:07 +0000

    started adding components for astral bodies (stars/planets/asteroids etc.)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use bevy::prelude::*;
 
+[#derive(Component)]
+struct Mass(f32);
+
+[#derive(Component)]
+struct Velocity(Vec3);
+
+[#derive(Component)]
+struct Accelleration(Vec3);
+
 fn main() {
     App::new()
         .add_system(hello_world)
