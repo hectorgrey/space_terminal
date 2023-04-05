622cd54cc7aca54d479e00898ea7c43a5f198e3f
hectorgrey <parker321@gmail.com>
2023-04-05 02:07:44 +0100

    added accelleration function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use bevy::prelude::*;
 
-[#derive(Component)]
+#[derive(Component)]
 struct Mass(f32);
 
-[#derive(Component)]
+#[derive(Component)]
 struct Velocity(Vec3);
 
-[#derive(Component)]
+#[derive(Component)]
 struct Accelleration(Vec3);
 
 fn main() {
@@ -17,4 +17,10 @@ fn main() {
 
 fn hello_world() {
     println!("Hello World!");
+}
+
+fn accellerate(mut query: Query<(&Mass, &mut Accelleration)>, force: Vec3) {
+    for (mass, mut accel) in query.iter_mut() {
+        accel.0 += force / mass.0;
+    }
 }
