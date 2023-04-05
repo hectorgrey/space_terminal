b3b5aaf8b1b7f51605a138b95cff9bb68e7a23d1
hectorgrey <parker321@gmail.com>
2023-04-05 02:31:17 +0100

    added a couple of placeholder locations

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Component)]
+struct Description(String);
+
+#[derive(Component)]
+struct Location;
+
 fn main() {
     App::new()
+        .add_startup_system(spawn_starting_location)
         .add_system(hello_world)
         .run()
 }
 
-fn hello_world() {
-    println!("Hello World!");
+fn hello_world(query: Query<(&Name, &Description, With<Location>)>) {
+    for (name, desc, _) in query.iter() {
+        println!("Hello, {}", name.0);
+        println!("{}", desc.0);
+    }
+}
+
+fn spawn_starting_location(mut commands: Commands) {
+    commands.spawn((
+        Location,
+        Name("Earth".into()),
+        Description("The bright blue marble is visible through the windows.".into())
+    ));
+    commands.spawn((
+        Location,
+        Name("The Sun".into()),
+        Description("A bright ball of incandescent gas is visible through the windows.\nSuch a view could potentially blind any who saw it".into())
+    ));
 }
